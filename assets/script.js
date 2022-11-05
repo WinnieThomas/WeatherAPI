6bac74f2aa6647f4d13f3075173556fd63d82c8b
WinnieThomas <winnieantony1988@gmail.com>
2022-11-05 23:01:46 +0000

    added API for current weather forecast

--- assets/script.js
+++ assets/script.js
@@ -12,17 +12,45 @@ const getLocation=() =>{
     }
 }
     const locationcheck=function(search){
-        const apiurl=`${WEATHER_API_URL}/geo/1.0/direct?q=${search}&limit=5&appid=${WEATHER_API_KEY}`;
+        var apiurl=`${WEATHER_API_URL}/geo/1.0/direct?q=${search}&limit=5&appid=${WEATHER_API_KEY}`;
         fetch(apiurl)
         .then(function(response){
             console.log(response);
             return response.json();})
            .then(function(data){
             console.log(data);
+            const location=data[0];
+            displayWeather(location);
            });
                 
          }
-        
+
+         
+    
+    const displayWeather=function(weatherData){
+    document.getElementById('inputlocation').innerHTML=`${weatherData.name},${weatherData.country}`;
+    getWeather(weatherData.lat,weatherData.lon);
+    }
+
+    const getWeather=function(lat,lon){
+       var apiurl=`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&exclude=minutely,hourly&appid=d91f911bcf2c0f925fb6535547a5ddc9`;
+        fetch(apiurl)
+        .then(function(response){
+         return response.json(); })
+        .then(function(data){
+            console.log(data);
+            displayWeathernow(data);
+            displayForecast(data);
+        });
+    }
+
+    const displayWeathernow=function(weatherData){
+        const currentData=weatherData.current;
+        document.getElementById('temperature-value').textContent=currentData.temp;
+        document.getElementById('wind-value').textContent=currentData.wind_speed;
+        document.getElementById('humidity-value').textContent=currentData.humidity;
+        document.getElementById('uv-index').textContent=currentData.uvi;
+    }
     
 
 const searchbutton=document.getElementById('search');
